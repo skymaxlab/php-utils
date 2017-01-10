bc12191f5183a19a4d8507e685d370784139c664
Yada Khov <yada.khov@gmail.com>
2017-01-10 04:25:42 -0800

    Add laravel support and laravel database.

--- readme.md
+++ readme.md
@@ -1,3 +1,19 @@
 # php-utils
 
 A collection of php utilities classes and functions.
+
+## load typical packages for all projects
+
+### Service Provider
+
+```php
+    Barryvdh\Debugbar\ServiceProvider::class,
+    Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+    Yadakhov\Laradump\LaradumpServiceProvider::class,
+```
+
+### Alias
+
+```php
+    'Debug' => Barryvdh\Debugbar\Facade::class,
+```
---
+++ src/Models/BaseModel.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace SkyMaxLab\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Yadakhov\InsertOnDuplicateKey;
+
+/**
+ * Class BaseModel.
+ */
+class BaseModel extends Model
+{
+    use InsertOnDuplicateKey;
+
+    /**
+     * Get an instance of the model.
+     *
+     * @return Model
+     */
+    public static function getInstance()
+    {
+        $class = get_called_class();
+
+        return new $class();
+    }
+
+    /**
+     * Get map of two columns.
+     *
+     * @param $column1
+     * @param $column2
+     *
+     * @return array
+     */
+    public static function getMap($column1, $column2)
+    {
+        $model = get_called_class();
+
+        $rows = $model::distinct()->select($column1, $column2)->get();
+
+        $out = [];
+
+        foreach ($rows as $row) {
+            $out[$row->{$column1}] = $row->{$column2};
+        }
+
+        return $out;
+    }
+}
